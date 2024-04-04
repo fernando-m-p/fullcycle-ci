9179db6842ba618da70fc0520502c08c640041f4
FERNANDO MORAIS PEIXOTO <fernando.peixoto@trt22.jus.br>
2024-04-04 12:11:33 -0300

    feat:adicionar testes das funções

--- src/domain/calculadora.spec.ts
+++ src/domain/calculadora.spec.ts
@@ -20,5 +20,45 @@ describe("calculadora unit test", ()=>{
 
     })
 
+    
+    it("Teste divisao", ()=>{
+        let calculadora = new Calculadora();
+        calculadora.divisao(10,2);
+        expect(calculadora.getResultado()).toBe(5);
+
+        calculadora.divisao2(10,2);
+        expect(calculadora.getResultado()).toBe(5);
+
+        calculadora.divisao3(10,2);
+        expect(calculadora.getResultado()).toBe(5);
+
+    })
+
+    
+    it("Teste restoDiv", ()=>{
+        let calculadora = new Calculadora();
+        calculadora.restoDiv(11,2);
+        expect(calculadora.getResultado()).toBe(1);
+        calculadora.restoDiv2(11,2);
+        expect(calculadora.getResultado()).toBe(1);
+        calculadora.restoDiv3(11,2);
+        expect(calculadora.getResultado()).toBe(1);
+
+    })
+
+    
+    it("Teste exponeciacao", ()=>{
+        let calculadora = new Calculadora();
+        calculadora.exponeciacao(2,3);
+        expect(calculadora.getResultado()).toBe(8);
+        calculadora.exponeciacao2(2,3);
+        expect(calculadora.getResultado()).toBe(8);
+        calculadora.exponeciacao3(2,3);
+        expect(calculadora.getResultado()).toBe(8);
+
+    })
+
+
+
 })
 
